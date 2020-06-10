b31fc1ce16b4a926b57eb3c0c98919ea79a548e3
annzy <annzyee@gmail.com>
2020-06-10 12:16:14 +0900

    test update

--- src/Practice1/SumAvg2.java
+++ src/Practice1/SumAvg2.java
@@ -1,10 +1,11 @@
 package Practice1;
+import java.util.*;
 
 public class SumAvg2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int i =0; i <5; i++) {
+		System.out.println("Hi");
+		}
 	}
-
 }
